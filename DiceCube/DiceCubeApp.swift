bddbbcf9f8848180a2c43fe36a4217ad625d0940
Pondd Sugthana <pondd.me@gmail.com>
2025-08-29 09:50:03 +0700

    Initial Commit

---
+++ DiceCube/DiceCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceCubeApp.swift
+//  DiceCube
+//
+//  Created by Pondd Air on 29/8/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct DiceCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceCubeTests/DiceCubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceCubeTests.swift
+//  DiceCubeTests
+//
+//  Created by Pondd Air on 29/8/2568 BE.
+//
+
+import Testing
+@testable import DiceCube
+
+struct DiceCubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
